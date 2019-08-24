8e0335197b52ad1ab3285d1ce3bac134bf725ecd
Matt D <mattd2003@gmail.com>
2019-08-24 16:18:19 -0700

    Initial commit.

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+#print(__file__)
+#print(os.path.dirname(__file__))
+#print(basedir)
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URL') or \
+	'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
